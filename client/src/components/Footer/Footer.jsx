ce1afb3fc947b118f33f53de0b22e11c1044aec1
niaemani <niaemanidickson@gmail.com>
2021-09-11 18:03:39 -0400

    setup Footer component

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import './Footer.css'
+
+const Footer = () =>
+  <footer>
+    <ul>
+      <li>
+        <img className='git-icons'></img>
+        <p className='TNDR-name'>Tiffany Diandra</p>
+      </li>
+      <li>
+        <p className='TNDR-name'>Nia-Emani</p>
+        <img className='git-icons'></img>
+      </li>
+      <li>©T-N-D-R</li>
+      <li>
+        <img className='git-icons'></img>
+        <p className='TNDR-name'>Daria Krestina</p>
+      </li>
+      <li>
+        <img className='git-icons'></img>
+        <p className='TNDR-name'>Rachel Ginnett</p>
+      </li>
+    </ul>
+  </footer>
+
+export default Footer
