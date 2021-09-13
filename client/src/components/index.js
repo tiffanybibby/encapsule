835f166ad6022d80942fd3fedd4a99752a5585c8
Tiffany Bibby <tiffanybibby@Tiffanys-MBP.fios-router.home>
2021-09-13 00:11:03 -0400

    components/index.js code and item cards files added

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,4 @@
 import Layout from './Layout/Layout'
 import Item from './Item/Item'
-import Search from './Search/Search'
-import Sort from './Sort/Sort'
 import ItemCards from './ItemCards/ItemCards'
-export { Layout, Item, Search, Sort, ItemCards }
+export { Layout, Item, ItemCards }
