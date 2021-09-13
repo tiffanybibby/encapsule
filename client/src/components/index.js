753ae5d989e87c032906285704e160d1056018cf
Tiffany Bibby <tiffanybibby@Tiffanys-MBP.fios-router.home>
2021-09-13 00:05:29 -0400

    components/index.js code

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+import Layout from './Layout/Layout'
+import Item from './Item/Item'
+import Search from './Search/Search'
+import Sort from './Sort/Sort'
+import ItemCards from './ItemCards/ItemCards'
+export { Layout, Item, Search, Sort, ItemCards }
