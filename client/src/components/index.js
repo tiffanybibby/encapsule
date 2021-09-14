cfc416780a93bfbd43d28c30e3ce6ce007aec40a
Tiffany Bibby <tiffanybibby@Tiffanys-MBP.fios-router.home>
2021-09-14 02:11:17 -0400

    css styling for nav, footer, item, itemCards, added global fonts, updated readme and githhub logo photos

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,4 +2,3 @@ import Layout from './Layout/Layout'
 import Item from './Item/Item'
 import ItemCards from './ItemCards/ItemCards'
 export { Layout, Item, ItemCards }
-import 'bootstrap/dist/css/bootstrap.min.css';
