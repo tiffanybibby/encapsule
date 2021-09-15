81568b4d1f7f614cf971318ea1e98fab32020c78
niaemani <niaemanidickson@gmail.com>
2021-09-15 00:40:52 -0400

    initial sign up css, fixed slogan on home page

--- client/src/screens/SignUp/SignUp.jsx
+++ client/src/screens/SignUp/SignUp.jsx
@@ -1,127 +1,142 @@
-import './SignUp.css'
-import { useState } from 'react'
-import { signUp } from '../../services/users'
-import { useHistory } from 'react-router-dom'
-import Layout from '../../components/Layout/Layout'
+import "./SignUp.css";
+import { useState } from "react";
+import { signUp } from "../../services/users";
+import { useHistory } from "react-router-dom";
+import Layout from "../../components/Layout/Layout";
 
 const SignUp = (props) => {
-  const history = useHistory()
+  const history = useHistory();
 
   const [form, setForm] = useState({
-    first_name: '',
-    last_name: '',
-    email: '',
-    password: '',
-    passwordConfirmation: '',
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
     isError: false,
-    errorMsg: '',
-  })
+    errorMsg: "",
+  });
 
-const handleChange = (event) =>
+  const handleChange = (event) =>
     setForm({
       ...form,
       [event.target.name]: event.target.value,
-    })
+    });
 
-const onSignUp = async (event) => {
-    event.preventDefault()
-    const { setUser } = props
-  try {
-    if (password === passwordConfirmation) { 
-      const user = await signUp(form)
-      setUser(user)
-      history.push('/items')
-    } else {
-      throw 'Sign Up Details Invalid'
+  const onSignUp = async (event) => {
+    event.preventDefault();
+    const { setUser } = props;
+    try {
+      if (password === passwordConfirmation) {
+        const user = await signUp(form);
+        setUser(user);
+        history.push("/items");
+      } else {
+        throw "Sign Up Details Invalid";
       }
     } catch (error) {
-      console.error(error)
+      console.error(error);
       setForm({
-        first_name: '',
-        last_name: '',
-        email: '',
-        password: '',
-        passwordConfirmation: '',
+        first_name: "",
+        last_name: "",
+        email: "",
+        password: "",
+        passwordConfirmation: "",
         isError: true,
-        errorMsg: 'Sign Up Details Invalid!',
-      })
+        errorMsg: "Sign Up Details Invalid!",
+      });
     }
-  }
+  };
 
-const renderError = () => {
-    const toggleForm = form.isError ? 'danger' : ''
+  const renderError = () => {
+    const toggleForm = form.isError ? "danger" : "";
     if (form.isError) {
-      return ( 
-      <>
-        <button type='submit'>Sign Up</button>
-        <p className={toggleForm}>
-          {form.errorMsg}
-        </p>
-      </>
-      )
+      return (
+        <>
+          <button className="signup-button" type="submit">
+            Sign Up
+          </button>
+          <p className={toggleForm}>{form.errorMsg}</p>
+        </>
+      );
     } else {
-      return <button type='submit'>Sign Up</button>
+      return (
+        <button className="signup-button" type="submit">
+          Sign Up
+        </button>
+      );
     }
-  }  
+  };
 
-const { first_name, last_name, email, password, passwordConfirmation } = form
+  const { first_name, last_name, email, password, passwordConfirmation } = form;
 
   return (
     <Layout>
-      <div className='sign-up'>
-        <div className='left-column'>
-          <img className='sign-up-img' src="https://lh3.googleusercontent.com/MJQaCBft3cJZSda5vhRDotuObepiBk-X4CrlAuw7_WKSwyTAYphb94Qg-5sb-d-sS0YprGds1rT3_SjFUBjyBbtgyCeBoSniFJ_sUwgZ0YHzb3ZJVJ315Lpld0mZ_vqISLf4X9dD4dE=h480" alt='Happy woman sitting in organized closet' />
-          <h1>Always know what to wear!</h1>
-          <h2>Easily keep track of your clothing with enCapsule.</h2>
+      <div className="sign-up">
+        <div className="left-column">
+          <img
+            className="signup-img"
+            src="https://lh3.googleusercontent.com/MJQaCBft3cJZSda5vhRDotuObepiBk-X4CrlAuw7_WKSwyTAYphb94Qg-5sb-d-sS0YprGds1rT3_SjFUBjyBbtgyCeBoSniFJ_sUwgZ0YHzb3ZJVJ315Lpld0mZ_vqISLf4X9dD4dE=h480"
+            alt="Happy woman sitting in organized closet"
+          />
+          <h1 className="signup-headline">Always know what to wear!</h1>
+          <h2 className="signup-caption">
+            Easily keep track of your clothing with enCapsule.
+          </h2>
         </div>
-        <div className='right-column'>
-          <form className='sign-up-form' onSubmit={onSignUp}>
-            <label>First Name</label>
+        <div className="right-column">
+          <form className="sign-up-form" onSubmit={onSignUp}>
+            <label className="signup-labels">First Name</label>
             <input
               required
-              type='text'
-              name='first_name'
+              type="text"
+              name="first_name"
               value={first_name}
               onChange={handleChange}
+              className="signup-input"
             />
-            <label>Last Name</label>
+            <label className="signup-labels">Last Name</label>
             <input
               required
-              type='text'
-              name='last_name'
+              type="text"
+              name="last_name"
               value={last_name}
               onChange={handleChange}
+              className="signup-input"
             />
-            <label>Email</label>
+            <label className="signup-labels">Email</label>
             <input
               required
-              type='email'
-              name='email'
+              type="email"
+              name="email"
               value={email}
               onChange={handleChange}
+              className="signup-input"
             />
-            <label>Password</label>
+            <label className="signup-labels">Password</label>
             <input
               required
-              type='password'
-              name='password'
+              type="password"
+              name="password"
               value={password}
               onChange={handleChange}
+              className="signup-input"
             />
-            <label>Password Confirmation</label>
+            <label className="signup-labels">Password Confirmation</label>
             <input
               required
-              type='password'
-              name='passwordConfirmation'
+              type="password"
+              name="passwordConfirmation"
               value={passwordConfirmation}
               onChange={handleChange}
+              className="signup-input"
             />
             {renderError()}
           </form>
         </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
